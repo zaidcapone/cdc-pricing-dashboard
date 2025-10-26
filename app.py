a41082039bf66e87e1edde1e1eac633c0117e861
zaidcapone <47106849+zaidcapone@users.noreply.github.com>
2025-10-26 16:46:40 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,7 @@
 import streamlit as st
+
 st.title("CDC Pricing Dashboard")
+st.write("Search prices")
+article = st.text_input("Enter article number")
+if st.button("Search"):
+    st.write(f"Searching: {article}")
